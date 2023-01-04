685e65d0b9f34024052dbf6e36a5da17400462e1
Josh Flynn <jflynnbusiness@gmail.com>
2023-01-03 21:33:17 -0600

    updated lighting and changed stars

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 import "./style.css";
 import * as THREE from "three";
-
+import { moon } from "./moon";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
+window.addEventListener("resize", () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+});
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
@@ -11,6 +16,7 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
+camera.position.z = 40;
 
 const renderer = new THREE.WebGL1Renderer({
   canvas: document.querySelector("#bg"),
@@ -18,47 +24,68 @@ const renderer = new THREE.WebGL1Renderer({
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.z = 40;
 
-const sphereGeo = new THREE.SphereGeometry(10);
-// const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const torusTexture = new THREE.TextureLoader().load("moon.jpg");
-const bumpTexture = new THREE.TextureLoader().load("normal.jpg");
-const material = new THREE.MeshStandardMaterial({
-  map: torusTexture,
-  normalMap: bumpTexture,
-});
-
-//this is the actual object displayed on the screen
-const moon = new THREE.Mesh(sphereGeo, material);
 scene.add(moon);
 
-const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add(ambientLight);
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(50, 50, 50);
+scene.add(pointLight);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.minDistance = 20;
 controls.maxDistance = 50;
 
-function addStar() {
-  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-  const material = new THREE.MeshStandardMaterial({
-    color: new THREE.Color("rgba(255,255,255,.2)"),
-  });
-  const star = new THREE.Mesh(geometry, material);
-
-  const [x, y, z] = Array(3)
-    .fill()
-    .map(() => THREE.MathUtils.randFloatSpread(100));
-
-  star.position.set(x, y, z);
-  scene.add(star);
-}
-Array(200).fill().forEach(addStar);
-
 const spaceTexture = new THREE.TextureLoader().load("space.jpg");
 scene.background = spaceTexture;
 
+const amount = 100000;
+const radius = 300;
+
+const positions = new Float32Array(amount * 3);
+const colors = new Float32Array(amount * 3);
+const sizes = new Float32Array(amount);
+
+const vertex = new THREE.Vector3();
+const color = new THREE.Color(0xffffff);
+
+for (let i = 0; i < amount; i++) {
+  vertex.x = (Math.random() * 2 - 1) * radius;
+  vertex.y = (Math.random() * 2 - 1) * radius;
+  vertex.z = (Math.random() * 2 - 1) * radius;
+  vertex.toArray(positions, i * 3);
+
+  color.toArray(colors, i * 3);
+
+  sizes[i] = 2;
+}
+
+const geometry = new THREE.BufferGeometry();
+geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
+geometry.setAttribute("customColor", new THREE.BufferAttribute(colors, 3));
+geometry.setAttribute("size", new THREE.BufferAttribute(sizes, 1));
+
+//
+
+const material = new THREE.ShaderMaterial({
+  uniforms: {
+    color: { value: new THREE.Color(0xffffff) },
+    pointTexture: {
+      value: new THREE.TextureLoader().load("spark1.png"),
+    },
+  },
+  vertexShader: document.getElementById("vertexshader").textContent,
+  fragmentShader: document.getElementById("fragmentshader").textContent,
+
+  blending: THREE.AdditiveBlending,
+  depthTest: false,
+  transparent: true,
+});
+
+//
+
+const sphere = new THREE.Points(geometry, material);
+scene.add(sphere);
+let sizeCount = 0;
 function animate() {
   requestAnimationFrame(animate);
 
---
+++ moon.js
@@ -0,0 +1,12 @@
+import * as THREE from "three";
+
+const sphereGeo = new THREE.SphereGeometry(10);
+const torusTexture = new THREE.TextureLoader().load("moon.jpg");
+const bumpTexture = new THREE.TextureLoader().load("normal.jpg");
+const material = new THREE.MeshStandardMaterial({
+  map: torusTexture,
+  normalMap: bumpTexture,
+});
+
+//this is the actual object displayed on the screen
+export const moon = new THREE.Mesh(sphereGeo, material);
